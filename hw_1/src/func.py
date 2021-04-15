179abf2b2bf977dcd9d79bb1bf020a03b55bbfa9
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-04-15 15:53:31 +0300

    дз доработанно

--- hw_1/src/func.py
+++ hw_1/src/func.py
@@ -1,32 +1,36 @@
 import math
 
+
 class Figure:
 
     def __init__(self):
-        self.area = 0
-
+        self.area = self.area_s()
 
     def add_area(self, figure):
-        if isinstance(self, Figure):
+        if isinstance(figure, Figure):
             return self.area + figure.area
         else:
             print('передан неправильный класс')
+
+    def area_s(self):
+        return self.area
 
 
 class Triangle(Figure):
     name = "Треугольник"
     angles = 3
 
-
     def __init__(self, a, b, c):
-        super().__init__()
+
         self.a = a
         self.b = b
         self.c = c
         self.perimetr = self.a + self.b + self.c
-        self.area = math.sqrt(self.perimetr * (self.perimetr - self.a) *
-                              (self.perimetr - self.b) * (self.perimetr - self.c))
+        super().__init__()
 
+    def area_s(self):
+        return math.sqrt(self.perimetr * (self.perimetr - self.a) *
+                         (self.perimetr - self.b) * (self.perimetr - self.c))
 
 
 class Rectangle(Figure):
@@ -34,11 +38,14 @@ class Rectangle(Figure):
     angles = 4
 
     def __init__(self, a, b):
-        super().__init__()
         self.a = a
         self.b = b
         self.perimetr = (self.a + self.b) * 2
-        self.area = self.a * self.b
+        super().__init__()
+
+
+    def area_s(self):
+        return self.a * self.b
 
 
 class Square(Figure):
@@ -46,10 +53,12 @@ class Square(Figure):
     angles = 4
 
     def __init__(self, a):
-        super().__init__()
         self.a = a
         self.perimetr = self.a * 4
-        self.area = self.a * self.a
+        super().__init__()
+
+    def area_s(self):
+        return self.a * self.a
 
 
 class Circle(Figure):
@@ -57,14 +66,12 @@ class Circle(Figure):
     angles = 0
 
     def __init__(self, r):
-        super().__init__()
         self.r = r
         self.perimetr = 2 * 3.14 * self.r
-        self.area = 3.14 * (self.r ** 2)
+        super().__init__()
+
+    def area_s(self):
+        return 3.14 * (self.r ** 2)
 
 
-s = Square(3)
-t = Triangle(2, 3, 4)
-print(s.area)
-print(t.area)
-print(type(t.add_area(s)), "----", t.add_area(s))
+
