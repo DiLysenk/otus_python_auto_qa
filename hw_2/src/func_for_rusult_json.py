b9628a80351ff7ae4952a5994a79f4c47e48ca47
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-04-23 13:39:41 +0300

    hw2

--- hw_2/src/func_for_rusult_json.py
+++ hw_2/src/func_for_rusult_json.py
@@ -1,13 +1,14 @@
+import json
 from json import loads, dumps
 import csv
 from csv import DictReader
 
-list_users = []
-list_books = []
-list_total = []
+list_users = [] # список с пользователями
+list_books = [] # список с книгами
+list_total = [] # результирующий список
 
 with open('../users-39204-8e2f95.json', 'r') as users_file:
-    # прочитаем файл
+    # прочитаем файл с users
     users_file_read = users_file.read()
     # прочитаное загрузим в users_loads
     users_loads = loads(users_file_read)
@@ -17,11 +18,13 @@ with open('../users-39204-8e2f95.json', 'r') as users_file:
 
         target = [item['name'], item['gender'], item['address']]
         dict_target = dict(zip(['name', 'gender', 'address'], target))
+        # Запишем значения в list_users
         list_users.append(dict_target)
 
 
 
 with open('../books-39204-271043.csv', 'r') as books_file:
+    # прочитаем файл с books
     reader = DictReader(books_file)
     header = reader.fieldnames[0:2]
     header.append(reader.fieldnames[3])
@@ -29,11 +32,11 @@ with open('../books-39204-271043.csv', 'r') as books_file:
     for row in reader:
         title = [row['Title'], row['Author'], row['Height']]
         book = dict(zip(header, title))
+        # Запишем значения в list_books
         list_books.append(book)
 
 
 # собираем users и books
-
 for _ in range(len(list_users)):
     if _ <= len(list_books):
         list_users[_].setdefault('books', [list_books[_]])
@@ -41,14 +44,6 @@ for _ in range(len(list_users)):
         list_users[_].setdefault('books', None)
     list_total.append(list_users[_])
 
-
-
-
-# список с книгами
-print('len______--', len(list_users), len(list_books))
-
-j = 0
-
-for i in list_total:
-    print(i)
-
+# Записываем файл
+with open("result_file.json", 'w') as result_file:
+    json.dump(list_total, result_file, indent=2)
