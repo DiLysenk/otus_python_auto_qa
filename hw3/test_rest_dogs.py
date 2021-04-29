1257061714d34e856ac94097eef3bc75a5b52914
dmitriy.lysenko <dmitriy.lysenko@gs-labs.ru>
2021-04-29 19:24:58 +0300

    add test for cerebrus

--- hw3/test_rest_dogs.py
+++ hw3/test_rest_dogs.py
@@ -1,5 +1,6 @@
 import pytest
 from jsonschema import validate
+import cerberus
 
 
 @pytest.mark.parametrize("end_point", ['https://dog.ceo/api/breeds/list/all',
@@ -11,19 +12,26 @@ def test_dog_api_status(response_get, end_point):
     assert response_get.status_code == 200
 
 
+# Валидация схемы
 @pytest.mark.parametrize("end_point", ['https://dog.ceo/api/breeds/list/all'])
 def test_dog_api_schema(response_get, end_point):
     schema = {
         "type": "object",
         'properties': {
-            'massage': {"type": "object"}
+            'message': {"type": "object"}
         }
-
     }
-
-
+    # validate возвращает None поэту assert не используем
     validate(instance=response_get.json(), schema=schema)
 
 
-# def test_dog_api_json(response_get):
-#     assert response_get.contetnt == "content"
+# Валидация схемы с помощью cerberus
+@pytest.mark.parametrize("end_point", ['https://dog.ceo/api/breeds/list/all'])
+def test_dog_api_schema_cerebrus(response_get, end_point):
+    schema_c = {
+        'message': {'type': 'dict'},
+        'status': {'type': 'string', } # валидация стринги статус, отделить схему от теста
+    }
+    v = cerberus.Validator()
+    assert v.validate(response_get.json(), schema_c)
+
